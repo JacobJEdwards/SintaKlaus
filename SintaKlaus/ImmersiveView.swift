d40ec261ed878ab493ee84a3faa1e8cb8d393192
Jacob Edwards <jacob.john1@talktalk.net>
2024-11-13 13:16:50 +0000

    Initial Commit

---
+++ SintaKlaus/ImmersiveView.swift
@@ -0,0 +1,28 @@
+//
+//  ImmersiveView.swift
+//  SintaKlaus
+//
+//  Created by Jacob Edwards on 13/11/2024.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+    @Environment(AppModel.self) var appModel
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .full) {
+    ImmersiveView()
+        .environment(AppModel())
+}
---
+++ SintaKlausTests/SintaKlausTests.swift
@@ -0,0 +1,17 @@
+//
+//  SintaKlausTests.swift
+//  SintaKlausTests
+//
+//  Created by Jacob Edwards on 13/11/2024.
+//
+
+import Testing
+@testable import SintaKlaus
+
+struct SintaKlausTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
